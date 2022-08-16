d5638681fad476f76b494efb20e706e416488623
Abhinav <abhinavsekar1515@gmail.com>
2022-08-16 16:01:22 +0000

    frontend link

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://chatap.vercel.app",
+    origin: "https://umoja-mu.vercel.app",
     // credentials: true,
   },
 });
